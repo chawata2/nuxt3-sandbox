6a87d22f36fd24a19971c4e9d3cb82424cf6a06a
chawata-wsl <cha39881@gmail.com>
2023-08-23 08:16:45 +0900

    WIP

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { FormValues, ChildValues } from '@/types/formValues'
+
+const child: ChildValues = {
+  child1: 'child text',
+  child2: false
+}
+const formValue = ref<FormValues>({
+  text1: 'default text',
+  text2: 1234,
+  childs: [child]
+})
+
+const onSubmit = () => {
+  console.log(formValue.value)
+}
+</script>
+
+<template>
+  <v-form v-model="valid" @submit.prevent="onSubmit">
+    <v-text-field v-model="formValue.text1"></v-text-field>
+    <v-text-field></v-text-field>
+    <v-btn type="submit">submit</v-btn>
+  </v-form>
+</template>
---
+++ types/formValues.ts
@@ -0,0 +1,10 @@
+export interface FormValues {
+  text1: string
+  text2: number
+  childs: ChildValues[]
+}
+
+export interface ChildValues {
+  child1: string
+  child2: boolean
+}
